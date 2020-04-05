5f1bad2133bea6f3d155699d583c53108bf36c2e
Bill Fienberg <billfienberg@gmail.com>
2020-04-05 18:49:25 -0500

    Add constructor and componentDidMount placeholders

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    console.log('Mounted')
+  }
+
   render() {
     return (
       <div className="App">
