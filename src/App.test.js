ee29ea9b6a16ee9495bab9161b080800828e5ff8
Bill Fienberg <billfienberg@gmail.com>
2020-04-06 18:52:08 -0500

    Add assertions for initial render

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,30 @@
 import React from "react"
-import { render } from "@testing-library/react"
+import { render, getByRole } from "@testing-library/react"
 import App from "./App"
 
 test("renders learn react link", () => {
-  const { getByText } = render(<App />)
-  const linkElement = getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+  const { getByRole, getByLabelText, queryByText } = render(<App />)
+
+  const usernameInput = getByLabelText(/username/i)
+  expect(usernameInput).toBeInTheDocument()
+  // TODO: assert that input box is empty
+
+  const fetchContributionsButton = getByRole(/button/i)
+  expect(fetchContributionsButton).toBeInTheDocument()
+  // TODO: assert that search button is disabled
+
+  const loadingMessage = queryByText(/loading/i)
+  expect(loadingMessage).not.toBeInTheDocument()
+  // TODO: assert that loading indicator is not present
+
+  const repoTable = queryByText(/loading/i)
+  expect(repoTable).not.toBeInTheDocument()
+  // TODO: assert that no repos are rendered
+
+  // TODO: input a username
+  // TODO: assert that search button is no longer disabled
+  // TODO: click search button
+  // TODO: assert that loading is present
+  // TODO: assert that loading disappears
+  // TODO: assert that repos are rendered
 })
