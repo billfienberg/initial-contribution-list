875d542589ad71b64e5543d0860d4817f9995625
Bill Fienberg <billfienberg@gmail.com>
2020-04-06 17:45:44 -0500

    Add access-token to headers via environment variable

--- src/index.js
+++ src/index.js
@@ -9,15 +9,26 @@ import { ApolloProvider } from "react-apollo"
 import { ApolloClient } from "apollo-client"
 import { createHttpLink } from "apollo-link-http"
 import { InMemoryCache } from "apollo-cache-inmemory"
+import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
   uri: "https://api.github.com/graphql",
 })
 
+const authLink = setContext((_, { headers }) => {
+  const token = process.env.REACT_APP_ACCESS_TOKEN
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
+
 const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache(),
-})
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
   <React.StrictMode>
