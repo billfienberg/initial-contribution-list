35a62504b1133da2e295f922845bdc2853b89150
Bill Fienberg <billfienberg@gmail.com>
2020-04-08 00:31:01 -0500

    Extract client out of index.js

--- src/index.js
+++ src/index.js
@@ -2,33 +2,10 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
-import * as serviceWorker from "./serviceWorker"
 
 // https://www.howtographql.com/react-apollo/1-getting-started/
 import { ApolloProvider } from "react-apollo"
-import { ApolloClient } from "apollo-client"
-import { createHttpLink } from "apollo-link-http"
-import { InMemoryCache } from "apollo-cache-inmemory"
-import { setContext } from "apollo-link-context"
-
-const httpLink = createHttpLink({
-  uri: "https://api.github.com/graphql",
-})
-
-const authLink = setContext((_, { headers }) => {
-  const token = process.env.REACT_APP_ACCESS_TOKEN
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    },
-  }
-})
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-})
+import client from "./client"
 
 ReactDOM.render(
   <React.StrictMode>
@@ -38,8 +15,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root"),
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
