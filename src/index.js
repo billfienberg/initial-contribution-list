608a11ecda5028b1dc30132d9957545cfe8d21d9
Bill Fienberg <billfienberg@gmail.com>
2020-04-10 01:40:01 -0500

    Delete Apollo stuff from index.js

--- src/index.js
+++ src/index.js
@@ -3,15 +3,9 @@ import ReactDOM from "react-dom"
 import App from "./App"
 import "bootstrap/dist/css/bootstrap.min.css"
 
-// https://www.howtographql.com/react-apollo/1-getting-started/
-import { ApolloProvider } from "react-apollo"
-import client from "./client"
-
 ReactDOM.render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root"),
 )
