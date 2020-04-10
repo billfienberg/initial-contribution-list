12a548a076add4ab632c334a7d6d98809a7cc7ca
Bill Fienberg <billfienberg@gmail.com>
2020-04-10 02:06:16 -0500

    Add comment with link to educational source

--- src/api.js
+++ src/api.js
@@ -8,6 +8,8 @@ function api(username) {
     variables,
   })
 
+  // Querying data using `fetch`
+  // https://www.prisma.io/tutorials/build-react-graphql-app-with-fetch-ct19/#querying-data-using-fetch
   return fetch("https://api.github.com/graphql", {
     method: "POST",
     headers: { "Content-Type": "application/json", Authorization: token ? `Bearer ${token}` : "" },
