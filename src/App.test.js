bf018e43a29676ad044aba2571b89a0e075ec4a4
Bill Fienberg <billfienberg@gmail.com>
2020-04-10 02:19:18 -0500

    Mock fetchReposContributedToByUser so our tests don't make network requests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from "react"
 import { waitForElementToBeRemoved } from "@testing-library/dom"
 import { render, fireEvent } from "@testing-library/react"
+import { fetchReposContributedToByUser as mockFetchReposContributedToByUser } from "./api"
+import mockData from "./mockData"
 import App from "./App"
 
+jest.mock("./api")
+
 test("renders the Contributions List app", async () => {
+  mockFetchReposContributedToByUser.mockResolvedValueOnce({ json: () => mockData })
   const { getByRole, getByLabelText, queryByText, getByTestId, queryByTestId } = render(<App />)
 
   expect(getByLabelText(/username/i).value).toBe("")
@@ -14,11 +19,15 @@ test("renders the Contributions List app", async () => {
 
   expect(queryByTestId(/repo-table/i)).not.toBeInTheDocument()
 
-  fireEvent.change(getByLabelText(/username/i), { target: { value: "billfienberg" } })
+  const username = "billfienberg"
+  fireEvent.change(getByLabelText(/username/i), { target: { value: username } })
 
   expect(getByRole(/button/i).disabled).toBe(false)
 
+  expect(mockFetchReposContributedToByUser).toHaveBeenCalledTimes(0)
   fireEvent.click(getByRole(/button/i))
+  expect(mockFetchReposContributedToByUser).toHaveBeenCalledTimes(1)
+  expect(mockFetchReposContributedToByUser).toHaveBeenCalledWith(username)
 
   await waitForElementToBeRemoved(() => queryByText(/loading/i))
   expect(queryByText(/loading/i)).not.toBeInTheDocument()
